d86844d58f0fab504e0ffda7e30be2d97afd31a3
User <giorgi.didebulidze@makingscience.com>
2023-11-29 11:35:57 +0400

    added constructor

--- Hermes/Services/UserService/UserService.Domain/Entities/User.cs
+++ Hermes/Services/UserService/UserService.Domain/Entities/User.cs
@@ -6,5 +6,18 @@
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public DateTime DateOfBirth { get; private set; }
+
+        private User()
+        {
+            
+        }
+
+        public User(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
     }
 }
