ceafe64b5117ae8fb72c81bf97a1074d1f6f7735
User <giorgi.didebulidze@makingscience.com>
2023-11-29 11:55:09 +0400

    added UserDto and maper to it

---
+++ Hermes/Services/UserService/UserService.Application/Dtos/UserDto.cs
@@ -0,0 +1,4 @@
+﻿namespace UserService.Application.Dtos
+{
+    public record UserDto(string FirstName, string LastName, DateTime DateOfBirth);
+}
---
+++ Hermes/Services/UserService/UserService.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UserService.Application.Dtos;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
