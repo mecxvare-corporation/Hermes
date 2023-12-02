8c8f5b93a832aaf6ffacca2f053ba1e5156bae6f
kombliani-komble <151441589+kombliani-komble@users.noreply.github.com>
2023-12-02 23:01:10 +0400

    Added logging middleware

---
+++ Hermes/Services/UserService/UserService.Api/Infrastructure/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Serilog;
+
+namespace UserService.Api.Infrastructure.Middlewares;
+
+public class ErrorHandlingMiddleware
+{
+    static readonly Serilog.ILogger Log = Serilog.Log.ForContext<ErrorHandlingMiddleware>();
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            Exception exception = GetInnermostExceptionMessage(ex);
+            Log.Fatal(exception, exception.Message);
+
+            string message = "An error occurred. Please try again later.";
+
+            if (exception != null)
+            {
+                message = exception.Message;
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "text/plain";
+            }
+
+            await context.Response.WriteAsJsonAsync(new { error = message });
+        }
+    }
+
+    public static Exception? GetInnermostExceptionMessage(Exception ex)
+    {
+        if (ex == null)
+        {
+            return null;
+        }
+
+        if (ex.InnerException == null)
+        {
+            return ex;
+        }
+
+        return GetInnermostExceptionMessage(ex.InnerException);
+    }
+}
