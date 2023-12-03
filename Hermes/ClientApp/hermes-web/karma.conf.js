b5d580db42cd7f6e4df0efbe3346c32c2e22ddd2
kombliani-komble <151441589+kombliani-komble@users.noreply.github.com>
2023-12-03 18:15:14 +0400

    Update karma.conf.js

--- Hermes/ClientApp/hermes-web/karma.conf.js
+++ Hermes/ClientApp/hermes-web/karma.conf.js
@@ -1,7 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -38,8 +35,9 @@ module.exports = function (config) {
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
-       base: 'ChromeHeadless',
-       flags: ['--no-sandbox']
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
     },
     restartOnFileChange: true
   });
