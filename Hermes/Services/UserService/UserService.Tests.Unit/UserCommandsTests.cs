85ea2b255b20d30fdf21738b17e133a46a145aec
User <giorgi.didebulidze@makingscience.com>
2023-12-04 18:22:38 +0400

    added test project and only one test

---
+++ Hermes/Services/UserService/UserService.Tests.Unit/UserCommandsTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MediatR;
+using Moq;
+using UserService.Application.Dtos;
+using UserService.Application.Users.Commands;
+using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Tests.Unit
+{
+    public class UserCommandsTests
+    {
+        [Fact]
+        public async Task CreateUserCommand_ShouldSucceed()
+        {
+            //Arrange
+            var mockMediator = new Mock<IMediator>();
+            var mockUow = new Mock<IUnitOfWork>();
+            var mockMapper = new Mock<IMapper>();
+            var createUserCommand = new CreateUserCommand(new CreateUserDto("TestName", "TestLastName", new DateTime()));
+            var createUserCommandHandler = new CreateUserCommandHandler(mockUow.Object, mockMapper.Object);
+
+            mockMapper.Setup(mapper => mapper.Map<User>(It.IsAny<CreateUserDto>())).Returns(new User("TestName", "TestLastName", new DateTime()));
+            //Act
+            await createUserCommandHandler.Handle(createUserCommand, CancellationToken.None);
+
+            //Assert
+            mockMediator.Verify(mediator=>mediator.Send(It.IsAny<CreateUserCommand>(), CancellationToken.None), Times.Once);
+            mockMapper.Verify(mapper => mapper.Map<User>(It.IsAny<CreateUserDto>()), Times.Once);
+
+        }
+
+        [Fact]
+        public void UpdateUserCommand_ShouldSucceed()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeleteUserCommand_ShouldSucceed() 
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
