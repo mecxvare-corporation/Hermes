157793eefe4a7a06c33ea97e599d9da18d24fbff
User <giorgi.didebulidze@makingscience.com>
2023-12-05 17:36:58 +0400

    added tests for User and UserCommands

---
+++ Hermes/Services/UserService/UserService.Tests.Unit/UserCommandShould.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using UserService.Application.Dtos;
+using UserService.Application.Users.Commands;
+using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Tests.Unit
+{
+    [Collection("MyCollection")]
+    public class UserCommandShould
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public UserCommandShould(ServiceProviderFixture fixture) 
+        {
+            _serviceProvider = fixture.ServiceProvider;
+        }
+
+        [Fact]
+        public async Task CreateNewUser()
+        {
+            //Arrange
+            var newUserDto = new CreateUserDto("test1", "test1", DateTime.Now);
+
+            // Mock IUserRepository
+            var userRepoMock = new Mock<IUserRepository>();
+
+            // Mock IUnitOfWork
+            var uofMcok = new Mock<IUnitOfWork>();
+            uofMcok.Setup(uow => uow.UserRepository).Returns(userRepoMock.Object);
+
+            var handler = new CreateUserCommandHandler(uofMcok.Object, _serviceProvider.GetRequiredService<IMapper>());
+            var query = new CreateUserCommand(newUserDto);
+
+            //Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            userRepoMock.Verify(repo=>repo.Create(It.IsAny<User>()), Times.Once);
+        }
+
+
+        [Fact]
+        public async void DeleteExistingUserById() 
+        {
+            //Arrange
+
+            // Mock IUserRepository
+            var userRepoMock = new Mock<IUserRepository>();
+
+            // Mock IUnitOfWork
+            var uofMcok = new Mock<IUnitOfWork>();
+            uofMcok.Setup(uow => uow.UserRepository).Returns(userRepoMock.Object);
+
+            var handler = new DeleteUserCommandHandler(uofMcok.Object);
+            var query = new DeleteUserCommand(Guid.NewGuid());
+
+            //Act
+            await handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            userRepoMock.Verify(repo=>repo.DeleteAsync(It.IsAny<Guid>()), Times.Once);
+        }
+    }
+}
---
+++ Hermes/Services/UserService/UserService.Tests.Unit/UserShould.cs
@@ -0,0 +1,54 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Tests.Unit
+{
+    [Collection("MyCollection")]
+    public class UserShould
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public UserShould(ServiceProviderFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+        }
+            
+        [Fact]
+        public void ConstructedWithValidData()
+        {
+            //Arrange
+            string name = "test1";
+            string lastName = "testTest";
+            DateTime birthDay = DateTime.Now;
+
+            //Act
+            var newUser = new User(name, lastName, birthDay);
+
+            //Assert
+            Assert.Equal(name, newUser.FirstName);
+            Assert.Equal(lastName, newUser.LastName);
+            Assert.Equal(birthDay.ToString(), newUser.DateOfBirth.ToString());
+        }
+
+        [Fact]
+        public void UpdateItselfById()
+        {
+            //Arrange
+            string name = "test1";
+            string lastName = "testTest";
+            DateTime birthDay = DateTime.Now;
+
+            var newUser = new User(name, lastName, birthDay);
+
+            string updatedName = "test2";
+            string updatedLastName = "testTest";
+            DateTime updatedBirthDay = DateTime.Now;
+
+            //Act
+            newUser.Update(updatedName, updatedLastName, updatedBirthDay);
+
+            //Assert
+            Assert.Equal(updatedName, newUser.FirstName);
+            Assert.Equal(updatedLastName, newUser.LastName);
+            Assert.Equal(updatedBirthDay.ToString(), newUser.DateOfBirth.ToString());
+        }
+    }
+}
