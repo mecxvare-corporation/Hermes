eee26bf8735be7aa78238d161e6c31d61983b6fb
bgigineishvili <bacho3111@gmail.com>
2023-12-06 17:56:19 +0400

    testing sonarcloud

---
+++ Hermes/Services/UserService/UserService.Infrastructure/Services/tEst_ClasS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Infrastructure.Services
+{
+    public class tEst_ClasS
+    {
+        public async void _tEsTMethoD()
+        {
+
+        }
+    }
+}
