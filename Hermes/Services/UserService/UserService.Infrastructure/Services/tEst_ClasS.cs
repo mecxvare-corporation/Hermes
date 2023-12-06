3024a908f912be1be56d330fd7e5d07b5ee3bd7c
bgigineishvili <bacho3111@gmail.com>
2023-12-06 20:16:58 +0400

    dasasdads

--- Hermes/Services/UserService/UserService.Infrastructure/Services/tEst_ClasS.cs
+++ Hermes/Services/UserService/UserService.Infrastructure/Services/tEst_ClasS.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace UserService.Infrastructure.Services
 {
     public class tEst_ClasS
     {
-        public async void _tEsTMethoD()
+        public async void _tEsTMettthoD()
         {
 
         }
