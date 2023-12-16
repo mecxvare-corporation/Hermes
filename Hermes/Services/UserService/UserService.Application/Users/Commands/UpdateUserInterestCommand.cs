a19a704a7ee4babee87eb0e85ed643f5f0d32311
Davit Abesalashvili <abesalashvili.dato@gmail.com>
2023-12-16 23:20:52 +0400

    Added tests to cover missing blocks

--- Hermes/Services/UserService/UserService.Application/Users/Commands/UpdateUserInterestCommand.cs
+++ Hermes/Services/UserService/UserService.Application/Users/Commands/UpdateUserInterestCommand.cs
@@ -27,7 +27,7 @@ namespace UserService.Application.Users.Commands
 
             var interests = await _unitOfWork.InterestRepository.GetRowsQueryable(x => request.Dto.InterestIds.Contains(x.Id), true, x => x.Users).ToListAsync(cancellationToken);
 
-            if (interests.Count == 0 || interests is null)
+            if (interests.Count == 0)
             {
                 throw new InvalidOperationException("Interests not found");
             }
