6253110f9e4b4a7d9325f7c84d672d1b5ded9a7d
User <giorgi.didebulidze@makingscience.com>
2024-01-10 12:25:53 +0400

    second try

--- Hermes/ClientApp/hermes-web/src/app/components/user/user.component.spec.ts
+++ Hermes/ClientApp/hermes-web/src/app/components/user/user.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserComponent } from './user.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from '../../services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -11,7 +12,7 @@ describe('UserComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserComponent, HttpClientModule],
-      providers: [UserService]
+      providers: [UserService, ActivatedRoute]
     })
     .compileComponents();
     
