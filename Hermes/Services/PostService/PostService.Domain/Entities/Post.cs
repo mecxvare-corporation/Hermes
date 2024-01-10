5caff9fc2470cd905de4a31b4823461e69ffd42d
User <giorgi.didebulidze@makingscience.com>
2024-01-10 18:11:54 +0400

    Added structure for post service

---
+++ Hermes/Services/PostService/PostService.Domain/Entities/Post.cs
@@ -0,0 +1,7 @@
+﻿namespace PostService.Domain.Entities
+{
+    public class Post : Entity
+    {
+        // TODO: Add needed properties
+    }
+}
---
+++ Hermes/Services/PostService/PostService.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace PostService.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IPostRepository PostRepository { get; }
+        Task<int> CompleteAsync();
+    }
+}
